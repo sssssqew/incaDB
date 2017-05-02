31bfb08464e1b371df64bd4852f4e5bce939844c
이성용 <sy.lee@dna.uno>
2017-05-02 18:08:45 +0900

    어드민에 보험 모델 관련 추가

--- incaDB/admin.py
+++ incaDB/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Fund, Price
+from .models import Fund, Price, Insurance
 
 # Register your models here.
 # Register your models here.
@@ -12,6 +12,11 @@ class FundAdmin(admin.ModelAdmin):
 class PriceAdmin(admin.ModelAdmin):
 	search_fields = ['Tradeday','Price', 'Upndown', 'created_date', 'updated_date']
 	ordering = ('fund', 'Tradeday',)
+
+class InsuranceAdmin(admin.ModelAdmin):
+	search_fields = ['FundCode','InsuType', 'ProdCode', 'SalesCode', 'ProdName', 'ProdFlag', 'ProdType', 'SaleSday', 'SaleEday', 'created_date', 'updated_date']
+	ordering = ('ProdName',)
 	
 admin.site.register(Fund, FundAdmin)
 admin.site.register(Price, PriceAdmin)
+admin.site.register(Insurance, InsuranceAdmin)
